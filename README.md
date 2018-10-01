683c00642724b1b301255e0318eeb01f214b8f69
Tushar <tusharnimje21@gmail.com>
2018-10-01 20:46:43 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Namespace.js
 Creates namespace for JavaScript applications
+
+```
+Namespace("app.test");
+app.test = {"name": "Hello"};
+
+Namespace("app.test.demo");
+
+```
---
+++ namespace.js
@@ -0,0 +1,28 @@
+var Namespace = (function(window) {
+  function _init(current, part, parent) {
+    if (current) {
+      return current;
+    } else {
+      parent[part] = {};
+      return parent[part];
+    }
+  }
+
+  function _namespace(identifier) {
+    var ns = window;
+    if (identifier !== "") {
+      var parts = identifier.split(Namespace.separator);
+      for (var i = 0, j = parts.length; i < j; i++) {
+        var current = ns[parts[i]];
+        ns = _init(current, parts[i], ns);
+      }
+    } else {
+      throw new Error("Identifier required");
+    }
+    return ns;
+  }
+
+  return _namespace;
+})(window);
+
+Namespace.separator = ".";
